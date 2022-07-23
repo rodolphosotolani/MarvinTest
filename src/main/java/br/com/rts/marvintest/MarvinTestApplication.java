af3131dd1e228310c87deafa944a95f2e5bc987b
rt.sotolani@gmail.com <Es@01062015>
2022-07-23 09:52:03 -0400

    Commit inicial.

---
+++ src/main/java/br/com/rts/marvintest/MarvinTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.rts.marvintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvinTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarvinTestApplication.class, args);
+    }
+
+}
