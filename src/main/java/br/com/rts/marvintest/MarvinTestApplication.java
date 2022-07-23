a3d5f210d0f1613cac6bc61336fdb957312f82b5
rt.sotolani@gmail.com <Es@01062015>
2022-07-23 10:08:06 -0400

    Adicionado Swagger e configurações

--- src/main/java/br/com/rts/marvintest/MarvinTestApplication.java
+++ src/main/java/br/com/rts/marvintest/MarvinTestApplication.java
@@ -2,8 +2,11 @@ package br.com.rts.marvintest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebMvc
 public class MarvinTestApplication {
 
     public static void main(String[] args) {
