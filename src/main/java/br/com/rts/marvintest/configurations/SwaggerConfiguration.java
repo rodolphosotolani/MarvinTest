f3be32d28d4fdd8b6439c1ce327f3e074d7997d1
rt.sotolani@gmail.com <Es@01062015>
2022-07-23 13:27:32 -0400

    Adicionado informações sobre a API no swagger e o redirecionamento do path / para a url do swagger.

--- src/main/java/br/com/rts/marvintest/configurations/SwaggerConfiguration.java
+++ src/main/java/br/com/rts/marvintest/configurations/SwaggerConfiguration.java
@@ -3,10 +3,14 @@ package br.com.rts.marvintest.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,9 +23,10 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.rts.marvintest.controllers"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(this.getApiInfo());
     }
 
     @Override
@@ -31,5 +36,30 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Case de teste de Rodolpho Teixeira Sotolani")
+                .description("Projeto base para teste de vaga Backend Java na empresa Marvin")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                        .contact(new Contact("Rodolpho T. Sotolani",
+                                "https://github.com/rodolphosotolani",
+                                "rt.sotolani@gmail.com"))
+                        .build();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry
+                .addViewController("/")
+                .setViewName("redirect:/swagger-ui.html");
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 }
