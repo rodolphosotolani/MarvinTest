ca783eef4432fd7c43af222a9bcefcf85120d868
rt.sotolani@gmail.com <Es@01062015>
2022-07-23 13:29:33 -0400

    Adicionado descritivo da API para o Swagger exibir uma API mais limpa e concisa. Criado endpoint para deletar usuario.

--- src/main/java/br/com/rts/marvintest/controllers/UsuarioController.java
+++ src/main/java/br/com/rts/marvintest/controllers/UsuarioController.java
@@ -5,11 +5,14 @@ import br.com.rts.marvintest.controllers.responses.UsuarioResponse;
 import br.com.rts.marvintest.domains.entity.Usuario;
 import br.com.rts.marvintest.mappers.UsuarioMapper;
 import br.com.rts.marvintest.services.UsuarioService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,6 +26,12 @@ public class UsuarioController {
     @Autowired
     private UsuarioMapper mapper;
 
+    @ApiOperation(value = "Buscar todos os Usuario")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna uma lista de usuarios"),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @GetMapping
     public ResponseEntity<List<UsuarioResponse>> getAllUsuario() {
 
@@ -35,6 +44,12 @@ public class UsuarioController {
                 .ok(mapper.entityToResponse(usuarioList));
     }
 
+    @ApiOperation(value = "Buscar Usuario por Id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna o Usuario selecionado"),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @GetMapping("/{idUsuario}")
     public ResponseEntity<UsuarioResponse> getAllUsuario(@RequestParam Long idUsuario) {
 
@@ -47,6 +62,12 @@ public class UsuarioController {
                 .ok(mapper.entityToResponse(usuario));
     }
 
+    @ApiOperation(value = "Cria um Usuario")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Usuario criado com sucesso"),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @PostMapping()
     public ResponseEntity<UsuarioResponse> createUsuario(@RequestBody UsuarioRequest usuarioRequest) {
 
@@ -57,9 +78,16 @@ public class UsuarioController {
             return ResponseEntity.notFound().build();
 
         return ResponseEntity
-                .ok(mapper.entityToResponse(usuario));
+                .status(HttpStatus.CREATED)
+                .body(mapper.entityToResponse(usuario));
     }
 
+    @ApiOperation(value = "Atualiza um Usuario")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Usuario atualizado com sucesso"),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
     @PutMapping("/{idUsuario}")
     public ResponseEntity<UsuarioResponse> updateUsuario(@RequestParam Long idUsuario,
                                                          @RequestBody UsuarioRequest usuarioRequest) {
@@ -73,6 +101,22 @@ public class UsuarioController {
 
         return ResponseEntity
                 .ok(mapper.entityToResponse(usuario));
+    }
+
+    @ApiOperation(value = "Deleta um Usuario")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "Usuario deletado com sucesso"),
+            @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso"),
+            @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+    })
+    @DeleteMapping("/{idUsuario}")
+    public ResponseEntity<Void> deleteUsuario(@RequestParam Long idUsuario) {
+
+        service.deleteUsuario(idUsuario);
+
+        return ResponseEntity
+                .accepted()
+                .build();
     }
 
 }
